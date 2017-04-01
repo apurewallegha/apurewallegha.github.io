ced6dd8afbb3c8a9df49012fd46df4dab38555a0
apurewallegha <amlegha@gmail.com>
2017-04-01 10:33:41 -0700

    Update index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,18 @@
-
+var food = ["grilled cheese sandwich", “spaghetti bolognese”, “cheese pizza”, “caesar salad”];
+undefined
+var foodObj = {
+    grilled cheese sandwich: {
+        ingredients: "bread, cheese",
+        nutrition: "40g carbohydrates"
+    },
+    spaghetti bolognese:  {
+        ingredients: "spaghetti, tomato, basil, oregano, olive oil, garlic, onion, salt, pepper",
+        nutrition: "220 carbohydrates"
+    },
+    cheese pizza:  {
+        ingredients: "pizza dough, tomato sauce, cheese",
+        nutrition: "223 carbohydrates"
+    },
+    caesar salad:  {
+        ingredients: "romaine lettuce, anchovy sauce, croutons",
+        nutrition: "360 carbohydrates"
